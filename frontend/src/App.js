40f92dc51dff8e725802940b90ec6391aedec57f
moshoodfakorede <moshood@armpension.com>
2021-03-12 20:18:28 +0100

    React Bootstrap and App Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
+import { Container } from "react-bootstrap";
+
+import Footer from "./components/Footer"
+import Header from "./components/Header";
+import HomePage from "./Pages/HomePage";
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to FabCommerce!</h1>
+      <Header />
+        <main className="py-3">
+          <Container>
+            <HomePage />
+          </Container>  
+        </main>
+      <Footer />
     </>
   );
 }
